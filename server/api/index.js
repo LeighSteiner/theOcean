3318f1279567f11c2f132904b94ea6cd2982e046
Leigh Steiner <steinerleigh@gmail.com>
2018-02-23 18:24:14 -0500

    blocked users added to session, ocean filter refactored accordingly

--- server/api/index.js
+++ server/api/index.js
@@ -1,27 +1,19 @@
 const router = require('express').Router()
-// const { BlockedUsers } = require('../db/models')
+const { BlockedUsers } = require('../db/models')
 module.exports = router
 let counter = 0
-// router.use((req, res, next) => {
-//   console.log('yo', counter++)
-//    if(req.user.blocks === undefined){
-//    	 BlockedUsers.findAll({where: {blocker: req.user.id}})
-//     .then((blockmatches) => {
-//       let badIds= []
-//        for(let i = 0; i < blockmatches.length; i++){
-//          badIds.push(blockmatches[i].dataValues.blockeeId);
-//        }
-//         req.user.blocks = badIds
-//        console.log('badIds', req.user.blocks)
-//        next();
 
-//     })
-//     .catch(next);
-//   }else{
-//   	console.log('blocked users')
-//   	 next();
-//   }
-// })
+//puts banned userIDs on the session
+router.use((req, res, next) => {
+ if(req.user){
+   	  BlockedUsers.findAll({where: {blocker: req.user.id }})
+      .then((blockMatches) => {
+  	  let blocks = blockMatches.map((el) => el.dataValues.blockeeId)
+  	  req.session.banned = blocks
+   })
+ }
+ next();
+})
 
 router.use('/users', require('./users'))
 router.use('/oceans', require('./oceans'))
