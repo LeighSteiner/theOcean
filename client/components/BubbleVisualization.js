1bf1bd64f1b5d9de599c96a79fdef1089b1a3fb0
Leigh Steiner <steinerleigh@gmail.com>
2018-03-14 17:46:48 -0400

    d3 force bubble chart implemented size change based on id NOT on date created

--- client/components/BubbleVisualization.js
+++ client/components/BubbleVisualization.js
@@ -9,15 +9,50 @@ export default class BubbleVisualization extends Component {
   componentDidMount(){
    //d3 shit goes here
    if(this.props.oceanBubbles){
-   	d3.select('#bubble-viz').selectAll('circle .bubbles')
-   	.data(this.props.oceanBubbles)
-   	.enter()
-   	.append('svg:circle')
-   	.attr('class', 'bubbles')
-   	.attr('r', '30')
-   	.attr('cx', '100')
-   	.attr('cy', '100')
-   	.style('fill', 'black')
+    //a radius scale to control radius of bubbles
+    //domain is the "raw input" that will be mapped on to a size range
+    let radiusScale = d3.scaleSqrt().domain([1, 35]).range([10, 80])
+
+    // a simulation is a collection of forces 
+    //detailing where we want our circles to go
+    // and how we want them to interact
+    //forceX moves us along the x axis to the parameter pt 
+    //(often say, width/2)
+    //step one: get them to middle
+    //step two: avoid collision  --use forceCollide
+    //step three: alter radius to represent data: right now it is set
+    //to show size based on id, alter later to do time since creation
+   	let simulation = d3.forceSimulation()
+   	.force("x", d3.forceX(200).strength(0.05))
+   	.force("y", d3.forceY(200).strength(0.05))
+   	.force("collide", d3.forceCollide((d) => radiusScale(d.id)))
+
+    let bubbles = this.props.oceanBubbles.map((bubble) => {
+     let bubbleObj = {
+     	id : bubble.id, 
+     	date: bubble.createdAt, 
+     	message: bubble.message, 
+     	userId: bubble.userId
+     }
+     return bubbleObj
+    })
+     let circles = d3.select('#bubble-viz').selectAll('circle .bubbles')
+   					.data(bubbles)
+   					.enter()
+  				 	.append('svg:circle')
+				   	.attr('class', 'bubbles dim')
+				   	.attr('id', (d) => 'bubble-' + d.id)
+				   	.attr('r', (d) => radiusScale(d.id))
+				   	.style('fill', '#b8c1bb')
+
+   	simulation.nodes(bubbles)
+   	  .on('tick', ticked)
+
+   	function ticked(){
+   	  circles
+   	    .attr('cx',(d) => d.x)
+   	    .attr('cy', (d) => d.y)
+   	}
    }
   }
 
@@ -25,12 +60,11 @@ export default class BubbleVisualization extends Component {
   	return false;
   }
 
-  componentWillReceiveProps(nextProps){
-
-  }
+//for complex updates?
+  // componentWillReceiveProps(nextProps){
+ // }
 
   render() {
-  	console.log('viz props', this.props.oceanBubbles)
    return <svg id='bubble-viz' ref='bubble-viz'/>
   }
 }
